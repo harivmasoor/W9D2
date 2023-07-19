476f3a51da85b179e8fc55287628733e532f5222
harivmasoor <harimasoor@gmail.com>
2023-07-18 17:32:33 -0700

    push

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,9 @@
-Add your import statements for View and Game here
+
+import View from "./ttt-view.js"
+import Game from "../ttt_node/game.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+ const game = new Game()
+ const ttt = document.querySelector('.ttt')
+ const view = new View(game, ttt)
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,17 +1,43 @@
 class View {
   constructor(game, el) {
+    this.game = game
+    this.el = el
+    this.setupBoard()
   }
   
   setupBoard() {
+    let gameBoard = document.createElement("ul")
+    gameBoard.classList.add("game-board")
+    for(let i=0; i<3; i++){
+      for(let j=0; j<3; j++){
+      let box = document.createElement("li")
+      box.classList.add("box")
+      gameBoard.appendChild(box)
+      box.dataset.position=JSON.stringify([i,j])
+    }
+    }
+    this.el.appendChild(gameBoard)
+    gameBoard.addEventListener("click", this.handleClick.bind(this))
   }
   
   handleClick(e) {
-  }
+    let pos = JSON.parse(e.target.dataset.position)
+    e.target.style.backgroundColor = "white"
+    e.target.style.fontSize = "180px"
+    e.target.innerHTML = this.game.currentPlayer
+    this.game.playMove(pos)
+    if(this.game.isOver()){
+      setTimeout(()=>alert(`Game Over!!!! The winner is ${this.game.winner()}`),500)
+      
+    }
+    }
 
   makeMove(square) {
+
   }
   
   handleGameOver() {
+
   }
 }
 
